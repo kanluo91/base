36a52952ffdb465e08f36b24378496226aed3488
金角大王 <kanluo91@yahoo.com>
2021-02-20 14:43:18 +0800

    数组的度

---
+++ Leetcode/697. 数组的度/697. 数组的度/main.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  697. 数组的度
+//
+//  Created by luokan on 2021/2/20.
+//  https://leetcode-cn.com/problems/degree-of-an-array/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int findShortestSubArray(vector<int>& nums) {
+        if(nums.size() == 0) return 0;
+        int size = (int)nums.size();
+        int dp[size];
+        memset(dp, 0, sizeof(int)*size);
+        
+        
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    
+    
+    return 0;
+}
